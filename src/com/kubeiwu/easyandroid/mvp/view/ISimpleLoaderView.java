9c5464ac03f0040ecdf02e105f2ba6691b8e3fce
Administrator <Administrator@PC-20150408YMYE>
2015-09-01 18:10:58 +0800

    Signed-off-by: Administrator <Administrator@PC-20150408YMYE>

--- src/com/kubeiwu/easyandroid/mvp/view/ISimpleLoaderView.java
+++ src/com/kubeiwu/easyandroid/mvp/view/ISimpleLoaderView.java
@@ -3,9 +3,7 @@ package com.kubeiwu.easyandroid.mvp.view;
 import android.os.Bundle;
 import android.support.v4.content.Loader;
 
-
-public interface ISimpleLoaderView<T> extends ISimpleView<T> ,IView {
+public interface ISimpleLoaderView<T> extends ISimpleView<T> {
 
 	public Loader<T> onCreateLoader(int arg0, Bundle bundle);
-
 }
