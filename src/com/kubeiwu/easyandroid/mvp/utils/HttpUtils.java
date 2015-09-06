8bf52b4c89e5b073ab659b0e4e3eb1f2962303ff
Administrator <Administrator@PC-20150408YMYE>
2015-09-06 16:40:54 +0800

    Signed-off-by: Administrator <Administrator@PC-20150408YMYE>

--- src/com/kubeiwu/easyandroid/mvp/utils/HttpUtils.java
+++ src/com/kubeiwu/easyandroid/mvp/utils/HttpUtils.java
@@ -15,7 +15,8 @@ import com.kubeiwu.easyandroid.kretrofit.KResult;
 import com.kubeiwu.easyandroid.kvolley.KGsonRequest;
 
 public class HttpUtils {
-	public static <T> Observable<T> futureToObservable(Future<T> future) {
+
+	public static <T> Observable<T> futureToObservable(final Future<T> future) {
 		Observable<T> observable = Observable.from(future, Schedulers.io());
 		return observable;
 	}
@@ -31,23 +32,21 @@ public class HttpUtils {
 		return future;
 	}
 
-	public static <T extends KResult> Observable<T> executeHttpRequestToObservable(int method, String url, Map<String, String> headers, Map<String, String> params, Type type, int requestMode) {
+	public static <T extends KResult> Observable<T> executeHttpRequestToObservable(final int method, final String url, final Map<String, String> headers, final Map<String, String> params, final Type type, final int requestMode) {
 		Future<T> future = voleyToFuture(method, url, headers, params, type, requestMode);
 		return futureToObservable(future);
 	}
 
 	public static <T extends KResult> Observable<T> executeHttpRequestToObservable(String url, Map<String, String> params, Type type, int requestMode) {
-		Future<T> future = voleyToFuture(Method.POST, url, null, params, type, requestMode);
-		return futureToObservable(future);
+		return executeHttpRequestToObservable(Method.POST, url, null, params, type, requestMode);
 	}
 
 	public static <T extends KResult> Observable<T> executeHttpRequestToObservable(String url, Type type, int requestMode) {
-		Future<T> future = voleyToFuture(Method.GET, url, null, null, type, requestMode);
-		return futureToObservable(future);
+		return executeHttpRequestToObservable(Method.POST, url, null, null, type, requestMode);
 	}
 
 	public static <T extends KResult> Observable<T> executeHttpRequestToObservable(String url, Type type) {
-		Future<T> future = voleyToFuture(Method.GET, url, null, null, type, RequestMode.LOAD_NETWORK_ONLY);
-		return futureToObservable(future);
+		return executeHttpRequestToObservable(Method.GET, url, null, null, type, RequestMode.LOAD_NETWORK_ONLY);
 	}
+
 }
