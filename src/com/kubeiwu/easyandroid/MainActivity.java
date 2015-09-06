99f701b67ec9d70bf03469f3fed036ffd548c8eb
Administrator <Administrator@PC-20150408YMYE>
2015-09-06 17:55:32 +0800

    Signed-off-by: Administrator <Administrator@PC-20150408YMYE>

--- src/com/kubeiwu/easyandroid/MainActivity.java
+++ src/com/kubeiwu/easyandroid/MainActivity.java
@@ -101,7 +101,7 @@ public class MainActivity extends FragmentActivity implements ISimpleNetWorkView
 	}
 
 	@Override
-	public Observable<JsonResult<List<AreaInfo>>> onCreatObservable() {
+	public Observable<JsonResult<List<AreaInfo>>> onCreatObservable(Bundle bundle) {
 		return api.getCity1();
 	}
 }
