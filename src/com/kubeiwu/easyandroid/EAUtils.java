f8d82332c660e9e362d2db12fc894fa49e7b94fa
cgpllx <cgpllx1@qq.com>
2015-10-18 18:06:44 +0800

    Signed-off-by: cgpllx <cgpllx1@qq.com>

---
+++ src/com/kubeiwu/easyandroid/EAUtils.java
@@ -0,0 +1,14 @@
+package com.kubeiwu.easyandroid;
+
+import android.content.Context;
+
+public class EAUtils {
+	public static void initHttpUtil(Context context) {
+		
+	}
+	
+	public static void initImageUtil(Context context) {
+		
+	}
+	
+}
